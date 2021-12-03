550cca86439d5ffb1c21ecd2321ea91289d05901
teohchinhowe <95393214+teohchinhowe@users.noreply.github.com>
2021-12-03 16:48:57 +0800

    add test api

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -35,5 +35,18 @@ namespace ActionDemo01.Controllers
             })
             .ToArray();
         }
+        
+        [HttpGet("test")]
+        public IEnumerable<WeatherForecast> Test()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
